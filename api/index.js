731bef174f9524576ff4120d36630d41813612cb
muddybootscode <firsttogo98@gmail.com>
2020-09-29 09:56:54 -0400

    Feature: more updates

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
-import { typeDefs } from "./graphql-schema";
-import { ApolloServer } from "apollo-server-express";
-import express from "express";
-import neo4j from "neo4j-driver";
-import { makeAugmentedSchema } from "neo4j-graphql-js";
-import dotenv from "dotenv";
+import { typeDefs } from './graphql-schema';
+import { ApolloServer } from 'apollo-server-express';
+import express from 'express';
+import neo4j from 'neo4j-driver';
+import { makeAugmentedSchema } from 'neo4j-graphql-js';
+import dotenv from 'dotenv';
 
 // set environment variables from .env
 dotenv.config();
@@ -11,13 +11,13 @@ dotenv.config();
 const app = express();
 
 const driver = neo4j.driver(
-  process.env.NEO4J_URI || "bolt://localhost:7687",
+  process.env.NEO4J_URI || 'bolt://localhost:7687',
   neo4j.auth.basic(
-    process.env.NEO4J_USER || "neo4j",
-    process.env.NEO4J_PASSWORD || "localgraph"
+    process.env.NEO4J_USER || process.env.LOCAL_NEO4J_USERNAME,
+    process.env.NEO4J_PASSWORD || process.env.LOCAL_NEO4J_PASSWORD
   ),
   {
-    encrypted: process.env.NEO4J_ENCRYPTED ? "ENCRYPTION_ON" : "ENCRYPTION_OFF",
+    encrypted: process.env.NEO4J_ENCRYPTED ? 'ENCRYPTION_ON' : 'ENCRYPTION_OFF',
   }
 );
 
@@ -41,8 +41,8 @@ const server = new ApolloServer({
 
 // Specify host, port and path for GraphQL endpoint
 const port = process.env.GRAPHQL_SERVER_PORT || 4001;
-const path = process.env.GRAPHQL_SERVER_PATH || "/graphql";
-const host = process.env.GRAPHQL_SERVER_HOST || "0.0.0.0";
+const path = process.env.GRAPHQL_SERVER_PATH || '/graphql';
+const host = process.env.GRAPHQL_SERVER_HOST || '0.0.0.0';
 
 server.applyMiddleware({ app, path });
 
