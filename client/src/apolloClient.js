52a9b1eb27e685922a2c91f94e0cb1de1d531940
muddybootscode <firsttogo98@gmail.com>
2020-09-29 09:32:37 -0400

    Feature: Apollo Client added to template

---
+++ client/src/apolloClient.js
@@ -0,0 +1,12 @@
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+
+const httpLink = new HttpLink({
+  uri: process.env.REACT_APP_GRAPHQL_URI || 'http://localhost:4000/graphql',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+export default client;
